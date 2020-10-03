1a7bc3c0f0d649b6ff91a34b022e6f22c3273867
sir-kitter <sire.kitter@gmail.com>
2020-10-03 14:17:36 +0300

    init: solve brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+const check = (string, bracketsConfig) => {
+    const brackets = new Map(bracketsConfig)
+    const expectations = [Infinity]
+    for(const char of string) {
+        if(char === expectations[expectations.length - 1])
+            expectations.pop()
+        else if(brackets.has(char))
+            expectations.push(brackets.get(char))
+        else
+            return false
+    }
+    return expectations.length === 1
 }
+
+module.exports = check
